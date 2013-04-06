289481f96240a5c46015ef5e697d23c522b958ef
이 재연 <nicola@jaeyeon-ui-MacBook-Pro.local>
2013-04-06 23:28:29 +0900

    첫 커밋임. 암것도 없음 ㅠㅠ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjouMu::Application.config.secret_token = '983d028706695152c7333a27d6beb68e993c2c805fc1498a43056179929f67f13ef1b7400a579ef2b0954988587ae20cc9812a11fd24e38185a1434c33800c11'
